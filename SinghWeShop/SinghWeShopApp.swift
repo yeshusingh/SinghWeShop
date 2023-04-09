e2cea4410bf3d76ff70e686485aa964fb4ed36be
Yeshu Singh <yeshusingh@outlook.com>
2023-04-09 10:45:20 -0700

    Initial Commit

---
+++ SinghWeShop/SinghWeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinghWeShopApp.swift
+//  SinghWeShop
+//
+//  Created by Yeshu Singh on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SinghWeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
